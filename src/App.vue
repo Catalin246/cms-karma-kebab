0e96f0a735edeebc7d73910108164429210b16db
Catalin246 <avorniceseicatalin1234@gmail.com>
2024-12-11 22:01:08 +0100

    refactor deploy.yml to streamline build process; remove cms-karma-kebab ConfigMap and update App.vue with project description

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
+    <p>This is the CMS for Karma Kebab project.</p>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
