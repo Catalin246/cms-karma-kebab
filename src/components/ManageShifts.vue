54767b6050938e7f5de77e588c400f7da4d16da1
Catalin246 <avorniceseicatalin1234@gmail.com>
2025-01-20 21:40:35 +0100

    Add button for adding shifts

--- src/components/ManageShifts.vue
+++ src/components/ManageShifts.vue
@@ -1,6 +1,13 @@
 <template>
     <div class="manage-shifts">
         <h2 class="text-2xl font-bold text-black">Manage Shifts</h2>
+        <div class="flex justify-between mb-4">
+            <button @click="openModal('add')" class="bg-green-500 text-white px-4 py-2 mt-3 rounded hover:bg-green-600">
+                Add Shift
+            </button>
+            <input v-model="searchEvent" type="text" placeholder="Search shifts"
+                class="p-2 border rounded text-black" />
+        </div>
         <div class="bg-white rounded-lg shadow border overflow-hidden">
             <table class="w-full">
                 <thead class="bg-gray-50">
--- src/components/ManageTasks.vue
+++ src/components/ManageTasks.vue
@@ -5,6 +5,8 @@
             <button @click="openModal('add')" class="bg-green-500 text-white px-4 py-2 mt-3 rounded hover:bg-green-600">
                 Add Task
             </button>
+            <input v-model="searchEvent" type="text" placeholder="Search shifts"
+                class="p-2 border rounded text-black" />
         </div>
         <div class="bg-white rounded-lg shadow border overflow-hidden">
             <table class="w-full">
