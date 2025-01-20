761fda95d11159ba5c90c558a4340467dff37ae9
Catalin246 <avorniceseicatalin1234@gmail.com>
2025-01-20 21:38:08 +0100

    Chsge the color of some text

--- src/components/UserAttemptLogin.vue
+++ src/components/UserAttemptLogin.vue
@@ -3,8 +3,8 @@
   <div class="h-screen w-full flex items-center justify-center bg-gray-800">
     <div class="text-center">
       <h1 class="text-3xl font-bold text-yellow-500 mb-4">Access Denied</h1>
-      <p class="text-xl mb-4">You do not have permission to access this CMS as a regular user.</p>
-      <p class="mb-4">If you believe you should have access, please contact an admin.</p>
+      <p class="text-xl text-white mb-4">You do not have permission to access this CMS as a regular user.</p>
+      <p class="mb-4 text-white">If you believe you should have access, please contact an admin.</p>
       <button 
         @click="requestAccess" 
         class="bg-yellow-500 text-white px-4 py-2 rounded hover:bg-yellow-600"
